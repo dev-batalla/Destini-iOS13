6f95548cf757c5cdb2ba29978b69eb0be445ce72
dev-batalla <jay.batalla@icloud.com>
2021-06-20 14:16:02 -0700

    added model and controller logic to complete project

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var story = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        if sender.tag == 0 {
+            story.queueNextStory(for: 1)
+            updateUI()
+        } else {
+            story.queueNextStory(for: 2)
+            updateUI()
+        }
+    }
+    
+    func updateUI() {
+        let current = story.getCurrentStory()
+        storyLabel.text = current.title
+        choice1Button.setTitle(current.choice1, for: .normal)
+        choice2Button.setTitle(current.choice2, for: .normal)
+    }
+    
 }
 
